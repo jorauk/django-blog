85792a35504693fdd727efe2c8500f91060e5f7c
Jonathan Mauk <jorauk@uw.edu>
2019-06-27 15:28:37 -0700

    Stubbed out customized ModelAdmin classes for Post and Category.

--- myblog/admin.py
+++ myblog/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+    
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Post)
 admin.site.register(Category)
