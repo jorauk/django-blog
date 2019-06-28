33d50a7e170d6d1372f34b00b7b1a8aa67962d40
Jonathan Mauk <jorauk@uw.edu>
2019-06-27 18:25:34 -0700

    Edited code to reference items in Django blog rather than the police example used on djangoproject.com.

--- myblog/feeds.py
+++ myblog/feeds.py
@@ -1,3 +1,17 @@
 from django.contrib.syndication.views import Feed
 from django.urls import reverse
-from policebeat.models import NewsItem
+from myblog.models import Post
+
+class LatestEntriesFeed(Feed):
+    title = "Latest posts"
+    link = "/latest/feed"
+    description = "The most recent posts on your blog"
+
+    def items(self):
+        return Post.objects.order_by('-published_date')[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.text
